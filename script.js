4938eda41b96a64a60a08884a46efa680c43f4a4
leila017 <leilanortje98@gmail.com>
2025-02-14 10:15:23 +0200

    added AI prompt and contexts

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function displayRecipe(response) {
-  //response.data.answer
   new Typewriter("#recipe", {
-    strings: "bla bla bla",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 80,
+    delay: 30,
     cursor: "",
   });
 }
 
 function generateRecipe(event) {
   event.preventDefault();
+  let instructions = document.querySelector("#user-instructions");
   let apiKey = "e14445dc5a1fa43f7b3b01otf4a4d0fb";
-  let context = "";
-  let prompt = "";
+  let context =
+    "You are a intellegent, friendly food expert, you love to suggest simple recepies. Generate using basic html. Please make sure to complete the recipe and Make sure to follow the user instructions.";
+  let prompt = `user instructions: Generate a recipe based on the users ${instructions.value}. Provide detailed instructions, estimated preparation time, and serving size.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
   axios.get(apiUrl).then(displayRecipe);
 }
 

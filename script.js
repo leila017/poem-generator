751a21d53f437be8f9e62f70f8d3337e7c5f003b
leila017 <leilanortje98@gmail.com>
2025-02-14 17:36:17 +0200

    improved the user interface

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function displayRecipe(response) {
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 30,
+    delay: 20,
     cursor: "",
   });
 }
@@ -12,9 +12,12 @@ function generateRecipe(event) {
   let instructions = document.querySelector("#user-instructions");
   let apiKey = "e14445dc5a1fa43f7b3b01otf4a4d0fb";
   let context =
-    "You are a intellegent, friendly food expert, you love to suggest simple recepies. Generate using basic html. Please make sure to complete the recipe and Make sure to follow the user instructions.";
+    "You are a intellegent, friendly food expert, you love to suggest simple recepies. Generate using basic html. Please make sure to complete the recipe, dont change font color and Make sure to follow the user instructions.";
   let prompt = `user instructions: Generate a recipe based on the users ${instructions.value}. Provide detailed instructions, estimated preparation time, and serving size.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeBlink = document.querySelector("#recipe");
+  recipeBlink.innerHTML = `<div class="generating">&#x231B; Please wait..</div>`;
   axios.get(apiUrl).then(displayRecipe);
 }
 
